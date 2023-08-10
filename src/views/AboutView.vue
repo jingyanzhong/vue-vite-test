57a10b7c83efc3e76b73b92592607d9186095cfa
jingyanzhong <melody804210@gmail.com>
2023-08-10 14:42:18 +0800

    git add

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,11 +2,13 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, eaque veritatis? Corporis reiciendis alias consequatur ad perspiciatis eum non officia velit deserunt quidem doloribus illum, iure aspernatur, expedita, at illo!</p>
+    <p>新增的一行文字</p>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
+
   .about {
     min-height: 100vh;
     display: flex;
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: 'vue-vite-test/',
+  base: '/vue-vite-test/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
